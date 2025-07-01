fc07ff0d086953441d30458ea44fd41fb5f4ee76
s2704287 <h.e.suteja@sms.ed.ac.uk>
2025-07-01 15:12:27 +0100

    add file version writing (for first entry for now)

--- userspace_app/userapp_writefile.c
+++ userspace_app/userapp_writefile.c
@@ -11,6 +11,7 @@
 #include <emmintrin.h> 
 #include <time.h>
 #include <x86intrin.h>
+#include <string.h>
 
 #define PAGE_SIZE       4096
 
@@ -18,51 +19,8 @@
 #define PAGE_COUNT (1 * 1024 * 1024 / PAGE_SIZE)
 #define ARRAY_COUNT (1 * 1024 * 1024 / sizeof(long unsigned int))
 
-void flush_cache() {
-    printf("trashing cache\n");
-    volatile char *trash = malloc(CACHE_TRASH_SIZE);
-
-    if (!trash) return;
-
-    for (size_t i = 0; i < CACHE_TRASH_SIZE; i += 64){
-        trash[i] = i;
-        _mm_mfence();
-    }
-
-    __asm__ volatile("mfence" ::: "memory");  // barrier biar gak dioptimasi
-    for (size_t i = 0; i < CACHE_TRASH_SIZE; i += 64) {
-        _mm_clflush((void *)&trash[i]);  // flush per cache line
-    }
-    __asm__ volatile("mfence" ::: "memory");
-
-    free((void *)trash);
-    printf("trashing cache done\n");
-
-}
-
-static __attribute__((always_inline)) inline uint64_t read_tsc_start(void) {
-    unsigned int lo, hi;
-    __asm__ volatile (
-        "cpuid\n\t"        // serialize sebelum RDTSC
-        "rdtsc\n\t"
-        : "=a"(lo), "=d"(hi)
-        : "a"(0)
-        : "%rbx", "%rcx");
-    return ((uint64_t)hi << 32) | lo;
-}
-
-static __attribute__((always_inline)) inline uint64_t read_tsc_end(void) {
-    uint32_t lo, hi;
-    __asm__ volatile (
-        "rdtscp\n\t"        // serialize setelah instruksi utama
-        "mov %%eax, %0\n\t"
-        "mov %%edx, %1\n\t"
-        "cpuid\n\t"         // serialize setelah RDTSC
-        : "=r"(lo), "=r"(hi)
-        :
-        : "%rax", "%rbx", "%rcx", "%rdx");
-    return ((uint64_t)hi << 32) | lo;
-}
+#define VERSION_SIZE (500 * 1024)
+#define VERSION_PAGE_COUNT (VERSION_SIZE / PAGE_SIZE)
 
 static __attribute__((always_inline)) inline uint64_t read_tsc(void) {
     unsigned int aux;
@@ -80,21 +38,60 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //uncached
+    printf("File name: %S\n", argv[1]);
+    printf("Version file name: %S\n", argv[2]);
+
+
     int fd = open(argv[1], O_RDWR);
     if (fd < 0) {
         perror("open");
         return 1;
     }
-    void *uc = mmap(NULL, PAGE_COUNT * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (uc == MAP_FAILED) {
-        perror("mmap");
-        printf("errono: %d\n", errno);
+    int version_fd = open(argv[2], O_RDWR);
+    if (version_fd < 0) {
+        perror("open version file");
         close(fd);
         return 1;
     }
 
-    printf("%s\n", argv[1]);
+    void *version_map = mmap(NULL, VERSION_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, version_fd, 0);
+    if (version_map == MAP_FAILED) {
+        perror("mmap version");
+        printf("errno: %d\n", errno);
+        close(version_fd);
+        close(fd);
+        return 1;
+    }
+
+    volatile char *version_entry = (volatile char *)version_map;
+    char *filepath_arg = (char *)malloc(strlen(argv[1]) + 1);
+    strcpy(filepath_arg, argv[1]);
+    char *filepath = strtok(filepath_arg, "/");
+    char *filename = (char *)malloc(strlen(argv[1]) + 1);
+    while(filepath != NULL) {
+        strncpy(filename, filepath, strlen(filepath));
+        snprintf(filename, strlen(filepath), "%s", filepath);
+        filepath = strtok(NULL, "/");
+    }
+    if (strlen(version_entry > 0)) {
+
+    } else {
+
+        strcpy(version_entry, "");
+    }
+
+    munmap(version_map, VERSION_SIZE);
+    close(version_fd);
+
+    void *uc = mmap(NULL, PAGE_COUNT * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (uc == MAP_FAILED) {
+        perror("mmap");
+        printf("errno: %d\n", errno);
+        munmap(version_map, VERSION_SIZE);
+        close(version_fd);
+        close(fd);
+        return 1;
+    }
 
     volatile long unsigned int *uc_int = (volatile long unsigned int *)uc;
     for (int i = 0; i < ARRAY_COUNT; i++) uc_int[i] = rand() % INT32_MAX;
@@ -103,14 +100,13 @@ int main(int argc, char *argv[]) {
         unsigned long tmp = 0;
         for (int i = 0; i < 3; i++) {
             __asm__ volatile("mfence" ::: "memory");
-            if(loop == 0) flush_cache();
             bef_uc = read_tsc();
             tmp = uc_int[loop];
             aft_uc = read_tsc();
             __asm__ volatile("mfence" ::: "memory");    
             sum += aft_uc - bef_uc;
         }
-        printf(" %d uncached cycle used: %lu. val %lu\n", loop + 1, sum, tmp);
+        printf(" %d cycle used: %lu. written value: %lu\n", loop + 1, sum, tmp);
     }
 
     munmap(uc, PAGE_COUNT * PAGE_SIZE);
