df4a6869af898aa28ed26b27a9ac622af64a9004
hanskoetjing <h.e.suteja@sms.ed.ac.uk>
2025-07-17 19:58:10 +0100

    update test mmap to loop 10x

--- userspace_app/test_mmap.c
+++ userspace_app/test_mmap.c
@@ -8,33 +8,35 @@
 int main() {
     int fd = open("/dev/cxl_mmap", O_RDWR | O_SYNC);
 
-    if (fd < 0) {
-        perror("open");
-        return 1;
-    }
+    for (int i = 0; i < 10; i++) {
+        if (fd < 0) {
+            perror("open");
+            return 1;
+        }
 
-    off_t phy_addr = 0x0;
-    size_t page_size = 4096;
+        off_t phy_addr = 0x0;
+        size_t page_size = 4096;
 
-    void *map_result = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, phy_addr);
+        void *map_result = mmap(NULL, page_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, phy_addr);
 
-    if (map_result == MAP_FAILED) {
-        perror("mmap");
-        printf("errono: %d\n", errno);
+        if (map_result == MAP_FAILED) {
+            perror("mmap");
+            printf("errono: %d\n", errno);
+            close(fd);
+            return 1;
+        }
+
+        printf("meow cxl addr %p\n", map_result);
+
+        volatile int *lala = (volatile int *)map_result;
+        printf("initial cxl val: %d\n", *lala);
+
+        *lala = i;
+
+        printf("after cxl %d \n", *lala);
+        
+        munmap(map_result, page_size);
         close(fd);
-        return 1;
     }
-
-    printf("meow cxl addr %p\n", map_result);
-
-    volatile int *lala = (volatile int *)map_result;
-    printf("initial cxl val: %d\n", *lala);
-
-    *lala = 0x0001;
-
-    printf("meow cxl %d \n", *lala);
-    
-    munmap(map_result, page_size);
-    close(fd);
     return 0;
 }
